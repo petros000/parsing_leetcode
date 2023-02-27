6e370a55252e321b00ed8d83348444f27d9d4254
petrs <petr.surovcev@gmail.com>
2023-02-28 00:35:28 +0300

    dint work status premium

--- get_data_problems.py
+++ get_data_problems.py
@@ -1,14 +1,17 @@
+import random
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import time
 from fake_useragent import UserAgent
 from bs4 import BeautifulSoup
+import json
 
 
 def get_html(url):
     useragent = UserAgent()
 
     options_browser = webdriver.ChromeOptions()
-    options_browser.add_argument(f"user-agent={useragent.random}")
+    options_browser.add_argument(f"user-agent={useragent.chrome}")
 
     browser = webdriver.Chrome(
         executable_path=f"drivers/chromedriver.exe",
@@ -17,8 +20,9 @@ def get_html(url):
 
     try:
         browser.get(url=url)
+        time.sleep(6)
         src_html = browser.page_source
-        time.sleep(5)
+
         return src_html
 
     except Exception as ex:
@@ -32,18 +36,51 @@ def get_html(url):
 def data_all_problems():
     data = dict()
     count = 0
-    for page in range(1, 2):
+    for page in range(1, 53):
         url_page = f"https://leetcode.com/problemset/all/?page={page}"
         page_html = get_html(url_page)
         soup = BeautifulSoup(page_html, "lxml")
-        tasks = soup.find_all(class_=['h-5', 'hover'])
+        tasks = soup.find_all(class_="odd:bg-layer-1 even:bg-overlay-1 dark:odd:bg-dark-layer-bg dark:even:bg-dark-fill-4")
+
         for task in tasks:
-            link = task.find("a").get("href")
-            print(link)
-    return tasks
+            if count == 0:
+                count += 1
+                continue
+
+            try:
+                is_prem = task.find_element(By.TAG_NAME, "path")
+                status_prem = "Yes"
+            except Exception:
+                status_prem = None
+
+            name = task.find(class_=['h-5']).text
+            link = "https://leetcode.com" + task.find("a").get("href")
+
+            data[str(count)] = {
+                "name": name,
+                "is_premium": status_prem,
+                "link": link,
+            }
+            count += 1
+        time.sleep(random.randrange(2, 5))
+
+    return data
 
 
 # url = "https://habr.com/ru/post/444460/"
 # html = get_html(url)
-tasks = data_all_problems()
-print(tasks)
+def main():
+    data = data_all_problems()
+    print(len(data))
+    with open('links_to_tasks.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
