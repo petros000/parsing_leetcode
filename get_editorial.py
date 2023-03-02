3c51d160431b6a75a7ea05ec533a8e17997ed310
petrs <petr.surovcev@gmail.com>
2023-03-02 17:10:42 +0300

    get editorial - processing.....

--- get_editorial.py
+++ get_editorial.py
@@ -2,14 +2,26 @@ from get_data_problems import get_html
 from bs4 import BeautifulSoup
 
 
-def is_solution_unlock(tag):
-    if tag:
-        return True
-    return False
+def get_editorial_solution(soup):
+    data_solution = dict()
+    tag_class = soup.find(class_="_16yfq _2YoR3")
+    contents = tag_class.contents
+    for content in contents:
+        if content.name == "h4":
+            cur_h4 = content.text
+            data_solution[cur_h4] = dict()
+
+        if content.name == "p":
+            if content.contents[0].name == "strong":
+                cur_strong = content.text
+                data_solution[cur_h4][cur_strong] = ""
+            else:
+                data_solution[cur_h4][cur_strong] += content.text
+
+    print(data_solution)
 
 
-def get_editorial_solution():
-    pass
+
 
 
 def get_data_editorial(url):
@@ -20,17 +32,19 @@ def get_data_editorial(url):
     page_html = get_html(url_page)
 
     soup = BeautifulSoup(page_html, "lxml")
-    tag_unlock = soup.find("a", string="Subscribe")
 
-    if is_solution_unlock(tag_unlock):
-        data_editorial["editorial solution"] = "unlock"
+    is_solution_unlock = True if soup.find("h4") is None else False
+
+    if is_solution_unlock:
+        data_editorial["editorial solution"] = None
+
     else:
-        if soup.find("div", class_="flex h-full w-full overflow-y-auto") is None:
-            data_editorial["editorial solution"] = None
-        else:
-            get_editorial_solution()
+        tag_edit_solutions = soup.find("div", class_="flex w-full flex-col gap-4 px-5 pb-8 pt-0")
+
+        get_editorial_solution(tag_edit_solutions)
 
 
 url1 = "https://leetcode.com/problems/triangle/"
 url2 = "https://leetcode.com/problems/text-justification/"
-get_data_editorial(url1)
+url3 = "https://leetcode.com/problems/two-sum/"
+get_data_editorial(url3)
